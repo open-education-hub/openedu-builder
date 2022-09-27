88512333589020f13b4a27ff881eaa09d1b826d4
Vlad Nastase <vladdnastase@gmail.com>
2022-09-27 18:31:17 +0300

    Add docusaurus plugin

--- openedu_builder/plugins/__init__.py
+++ openedu_builder/plugins/__init__.py
@@ -1,9 +1,11 @@
 from openedu_builder.plugins.command import CommandPlugin
 from openedu_builder.plugins.revealmd import RevealMdPlugin
 from openedu_builder.plugins.embed_reveal import EmbedRevealPlugin
+from openedu_builder.plugins.docusaurus import DocusaurusPlugin
 
 plugins = {
     "command": CommandPlugin,
     "revealmd": RevealMdPlugin,
-    "reveal_embed": EmbedRevealPlugin
+    "reveal_embed": EmbedRevealPlugin,
+    "docusaurus": DocusaurusPlugin
 }
