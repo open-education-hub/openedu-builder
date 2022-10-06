921feda029086973b493e5489258b89978601754
Vlad Nastase <vladdnastase@gmail.com>
2022-10-06 14:53:59 +0300

    [command] allow only relative paths in locations

--- openedu_builder/plugins/command.py
+++ openedu_builder/plugins/command.py
@@ -14,12 +14,21 @@ log = logging.getLogger(__name__)
 class CommandPlugin(Plugin):
     def __init__(self, input_dir: str, output_dir: str, config: Mapping[str, Any]):
         super().__init__(input_dir, output_dir, config)
+
         self.locations = config.get("locations")
+        if any([os.path.isabs(location) for location in self.locations]):
+            log.error(f"One or more absolute locations: \n {self.locations}")
+            raise PluginRunError(
+                "The locations to run commands must be relative to the input directory"
+            )
 
     def run(self):
         # The command plugin copies the input directory in the output directory so it can run the
-        # command in an isolated manner. We need to avoid copying the build directory if it is 
+        # command in an isolated manner. We need to avoid copying the build directory if it is
         # situated inside the input directory because that would create a cycle.
+        # TODO: Is there a way to figure this out for Docker container mappings as well?
+        # Currently, if the build output directory is under the input directory, but it is mapped
+        # inside a container somewhere outside the input directory, this will still cause a cycle.
         def ignore_build(dir, files):
             ret = []
             for file in files:
@@ -48,7 +57,7 @@ class CommandPlugin(Plugin):
                 path = location
             else:
                 path = os.path.join(self.output_dir, location)
-                
+
             os.chdir(path)
 
             cmd = [self.config["command"], *self.config["args"]]
